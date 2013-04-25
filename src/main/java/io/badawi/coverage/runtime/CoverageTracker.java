adcb5d3154c330d1ac27b33618c08a050cb2bc83
Ismail Badawi <isbadawi@gmail.com>
2013-04-25 00:54:35 -0400

    Remove Guava dependency in CoverageTracker.
    (Also rename className to filename in a few places.)

--- src/main/java/io/badawi/coverage/runtime/CoverageTracker.java
+++ src/main/java/io/badawi/coverage/runtime/CoverageTracker.java
@@ -1,17 +1,13 @@
 package io.badawi.coverage.runtime;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Throwables;
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
-import com.google.common.io.Files;
-
 public class CoverageTracker {
-  private static Table<String, Integer, Boolean> coverage = HashBasedTable.create();
+  private static Map<String, Map<Integer, Boolean>> coverage =
+      new HashMap<String, Map<Integer, Boolean>>();
 
   static {
     Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -23,18 +19,26 @@ public class CoverageTracker {
 
   private static void writeCoverageToFile() {
     String lcovCoverage = generateLcov();
+    FileWriter writer = null;
     try {
-      Files.write(lcovCoverage, new File("coverage_report.lcov"), Charsets.UTF_8);
+      writer = new FileWriter("coverage_report.lcov");
+      writer.write(lcovCoverage);
     } catch (IOException e) {
-      throw Throwables.propagate(e);
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        writer.close();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
     }
   }
 
   private static String generateLcov() {
     StringBuilder sb = new StringBuilder();
-    for (String className : coverage.rowKeySet()) {
+    for (String className : coverage.keySet()) {
       sb.append("SF:" + className + "\n");
-      for (Map.Entry<Integer, Boolean> line : coverage.row(className).entrySet()) {
+      for (Map.Entry<Integer, Boolean> line : coverage.get(className).entrySet()) {
         sb.append(String.format("DA:%d,%d\n", line.getKey(), line.getValue() ? 1 : 0));
       }
       sb.append("end_of_record\n");
@@ -42,51 +46,57 @@ public class CoverageTracker {
     return sb.toString();
   }
 
-  public static void markExecutable(String className, int line) {
-    coverage.put(className, line, false);
+  public static void markExecutable(String filename, int line) {
+    if (!coverage.containsKey(filename)) {
+      coverage.put(filename, new HashMap<Integer, Boolean>());
+    }
+    coverage.get(filename).put(line, false);
   }
 
-  public static void markExecuted(String className, int line) {
-    coverage.put(className, line, true);
+  public static void markExecuted(String filename, int line) {
+    if (!coverage.containsKey(filename)) {
+      coverage.put(filename, new HashMap<Integer, Boolean>());
+    }
+    coverage.get(filename).put(line, true);
   }
 
-  public static byte markExecuted(String className, int line, byte expression) {
-    markExecuted(className, line);
+  public static byte markExecuted(String filename, int line, byte expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static char markExecuted(String className, int line, char expression) {
-    markExecuted(className, line);
+  public static char markExecuted(String filename, int line, char expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static int markExecuted(String className, int line, int expression) {
-    markExecuted(className, line);
+  public static int markExecuted(String filename, int line, int expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static long markExecuted(String className, int line, long expression) {
-    markExecuted(className, line);
+  public static long markExecuted(String filename, int line, long expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static boolean markExecuted(String className, int line, boolean expression) {
-    markExecuted(className, line);
+  public static boolean markExecuted(String filename, int line, boolean expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static float markExecuted(String className, int line, float expression) {
-    markExecuted(className, line);
+  public static float markExecuted(String filename, int line, float expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static double markExecuted(String className, int line, double expression) {
-    markExecuted(className, line);
+  public static double markExecuted(String filename, int line, double expression) {
+    markExecuted(filename, line);
     return expression;
   }
 
-  public static <T> T markExecuted(String className, int line, T expression) {
-    markExecuted(className, line);
+  public static <T> T markExecuted(String filename, int line, T expression) {
+    markExecuted(filename, line);
     return expression;
   }
 }
